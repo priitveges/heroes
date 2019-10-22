2b37609d5eb8540539be23c62a815e7098415705
pveges <priit.veges@gmail.com>
2019-10-22 14:14:05 +0300

    Fixed Swagger

--- src/main/java/ee/inbank/heroes/config/SwaggerConfig.java
+++ src/main/java/ee/inbank/heroes/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
   public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2)
         .select()
-        .apis(RequestHandlerSelectors.basePackage("ee.taltech"))
+        .apis(RequestHandlerSelectors.basePackage("ee.inbank"))
         .paths(PathSelectors.any())
         .build();
   }
