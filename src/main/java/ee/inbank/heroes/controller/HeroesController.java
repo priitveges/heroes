5600af366e29cb67fa5a1ccbba238e4d09ca9dbe
pveges <priit.veges@gmail.com>
2019-10-22 13:33:51 +0300

    Add heroes controller

--- src/main/java/ee/inbank/heroes/controller/HeroesController.java
+++ src/main/java/ee/inbank/heroes/controller/HeroesController.java
@@ -1,5 +1,17 @@
 package ee.inbank.heroes.controller;
 
+import ee.inbank.heroes.dto.Hero;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HeroesController {
 
+  @GetMapping("/heroes")
+  public List<Hero> getHeroes() {
+    return Arrays.asList(new Hero("Priit", 100, 100),
+        new Hero("Oleg", 50, 80));
+  }
 }
--- src/main/java/ee/inbank/heroes/dto/Hero.java
+++ src/main/java/ee/inbank/heroes/dto/Hero.java
@@ -1,6 +1,11 @@
 package ee.inbank.heroes.dto;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Hero {
 
   public String name;
+  public Integer strenght;
+  public Integer stamina;
 }
