fc8acb4d2382260d8c6253bda6227f4720437911
pveges <priit.veges@gmail.com>
2019-10-22 14:00:23 +0300

    Use db in our service

--- src/main/java/ee/inbank/heroes/service/HeroesService.java
+++ src/main/java/ee/inbank/heroes/service/HeroesService.java
@@ -1,15 +1,19 @@
 package ee.inbank.heroes.service;
 
 import ee.inbank.heroes.model.Hero;
+import ee.inbank.heroes.repository.HeroesRepository;
 import java.util.Arrays;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class HeroesService {
 
+  private HeroesRepository heroesRepository;
+
   public List<Hero> getHeroes() {
-    return Arrays.asList(new Hero("Priit", 100, 100),
-        new Hero("Oleg", 50, 80));
+    return heroesRepository.findAll();
   }
 }
